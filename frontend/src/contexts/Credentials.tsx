2094e91dd04676e6e133dae8d3089e1b2a0cb808
foobar <uagnl25x@anonaddy.me>
2023-05-14 19:27:52 +0530

    New setters for changing user credentials in context provider

--- frontend/src/contexts/Credentials.tsx
+++ frontend/src/contexts/Credentials.tsx
@@ -3,21 +3,32 @@ import React, { createContext, useState } from 'react'
 export const CredentialContext = createContext({
     username: '',
     isLoggedIn: false,
-    handleCredentialChange: (username: string) => {},
+    accessToken: '',
+    setUserCredentials: (username: string, accessToken: string) => {},
+    setUserUsername: (username: string) => {},
+    setUserAccessToken: (accessToken: string) => {},
 })
 
 export const CredentialContextProvider = (props: React.PropsWithChildren) => {
     const [credential, setCredentials] = useState({
         username: '',
         isLoggedIn: false,
+        accessToken: '',
     })
 
-    const handleCredentialChange = (username: string) => {
-        setCredentials({ username, isLoggedIn: true })
+    const setUserCredentials = (username: string, accessToken: string) => {
+        setCredentials({ username, accessToken, isLoggedIn: true })
+    }
+    const setUserUsername = (username: string) => {
+        setCredentials(prev => ({ ...prev, username, isLoggedIn: true }))
+    }
+
+    const setUserAccessToken = (accessToken: string) => {
+        setCredentials(prev => ({ ...prev, accessToken, isLoggedIn: true }))
     }
 
     return (
-        <CredentialContext.Provider value={{ ...credential, handleCredentialChange }}>
+        <CredentialContext.Provider value={{ ...credential, setUserCredentials, setUserUsername, setUserAccessToken }}>
             {props.children}
         </CredentialContext.Provider>
     )
