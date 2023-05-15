f82bd8a9b81db648b14768495fe13ba1b6d604a6
foobar <uagnl25x@anonaddy.me>
2023-05-15 23:49:29 +0530

    Added ability to restore sessions using localStorage

--- frontend/src/contexts/Credentials.tsx
+++ frontend/src/contexts/Credentials.tsx
@@ -9,22 +9,43 @@ export const CredentialContext = createContext({
     setUserAccessToken: (accessToken: string) => {},
 })
 
+export interface Credentials {
+    username: string
+    isLoggedIn: boolean
+    accessToken: string
+}
+
 export const CredentialContextProvider = (props: React.PropsWithChildren) => {
-    const [credential, setCredentials] = useState({
+    const storedCredentials = window.localStorage.getItem('credentials')
+    console.log('valueare:',storedCredentials)
+
+    let restoredCredentials = {
         username: '',
         isLoggedIn: false,
         accessToken: '',
-    })
+    }
+
+    if (typeof storedCredentials === 'string') restoredCredentials = JSON.parse(storedCredentials)
+
+    const [credential, setCredentials] = useState<Credentials>(restoredCredentials)
 
     const setUserCredentials = (username: string, accessToken: string) => {
         setCredentials({ username, accessToken, isLoggedIn: true })
+        updateLocalStorage()
     }
     const setUserUsername = (username: string) => {
         setCredentials(prev => ({ ...prev, username, isLoggedIn: true }))
+        updateLocalStorage()
     }
 
     const setUserAccessToken = (accessToken: string) => {
         setCredentials(prev => ({ ...prev, accessToken, isLoggedIn: true }))
+        updateLocalStorage()
+    }
+
+    const updateLocalStorage = () => {
+        window.localStorage.setItem('credentials', JSON.stringify(credential))
+        console.log('setting items as', credential)
     }
 
     return (
