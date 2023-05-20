f5c43987cb8d2bd875a9bbac4710effb415af089
foobar <uagnl25x@anonaddy.me>
2023-05-20 11:10:35 +0530

    Added comments

--- backend/src/middleware/jwtFunctions.ts
+++ backend/src/middleware/jwtFunctions.ts
@@ -3,12 +3,14 @@ import { NextFunction, Request, Response } from 'express'
 
 type TokenType = 'refresh' | 'access'
 
+// check if refresh/access token is valid
 export const isTokenValid = (token: string, tokenAccessOrRefresh: TokenType) => {
     const tokenType =
         tokenAccessOrRefresh === 'access' ? process.env.ACCESS_TOKEN_SECRET : process.env.REFRESH_TOKEN_SECRET
     const fa = jwt.verify(token, tokenType)
 }
 
+// checks if accessToken is valid
 export const checkAccessTokenValidity = (req: Request, res: Response, next: NextFunction) => {
     const { authorization } = req.headers
 
