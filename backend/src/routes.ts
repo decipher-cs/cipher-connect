631188a50c90e86be64e215276de9a5018207590
foobar <uagnl25x@anonaddy.me>
2023-05-20 11:11:23 +0530

    Added New Route To Varify User Cookie

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,9 +1,10 @@
 import { Express } from 'express'
-import { createUser, loginUser, logoutUser, renewAccessToken } from './controllers.js'
+import { createUser, loginUser, logoutUser, renewAccessToken, varifyRefreshToken } from './controllers.js'
 
 export const initRoutes = (app: Express) => {
     app.all('/login', loginUser)
     app.all('/signup', createUser)
     app.all('/renewtoken', renewAccessToken)
     app.all('/logout', logoutUser)
+    app.all('/varifyRefreshToken', varifyRefreshToken)
 }
