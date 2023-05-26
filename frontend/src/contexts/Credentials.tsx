cd15013a937b0a273a14ca1dc12913851646c72e
foobar <uagnl25x@anonaddy.me>
2023-05-26 14:45:17 +0530

    Added handler for changing the entire user credentials or parts of it

--- frontend/src/contexts/Credentials.tsx
+++ frontend/src/contexts/Credentials.tsx
@@ -1,29 +1,39 @@
 import React, { createContext, useState } from 'react'
 
+type RequireAtLeastOne<T, Keys extends keyof T = keyof T> = Pick<T, Exclude<keyof T, Keys>> &
+    {
+        [K in Keys]-?: Required<Pick<T, K>> & Partial<Pick<T, Exclude<Keys, K>>>
+    }[Keys]
+
+type CredentialKey = RequireAtLeastOne<Credentials>
+
 export const CredentialContext = createContext({
     username: '',
+    sessionId: '',
     isLoggedIn: false,
-    setUserUsername: (username: string) => {},
+    handleCredentialChange: (newCredential: CredentialKey) => {},
 })
 
 export interface Credentials {
     username: string
+    sessionId: string
     isLoggedIn: boolean
 }
 
 export const CredentialContextProvider = (props: React.PropsWithChildren) => {
     const [credential, setCredentials] = useState<Credentials>({
         username: '',
+        sessionId: '',
         isLoggedIn: false,
     })
 
-    const setUserUsername = (username: string) => {
-        setCredentials(prev => ({ ...prev, username, isLoggedIn: true }))
-        window.localStorage.setItem('username', username)
+    const handleCredentialChange = (newCredential: CredentialKey) => {
+        setCredentials(prev => ({ ...prev, ...newCredential }))
+        if (newCredential?.username !== undefined) window.localStorage.setItem('username', newCredential.username)
     }
 
     return (
-        <CredentialContext.Provider value={{ ...credential, setUserUsername }}>
+        <CredentialContext.Provider value={{ ...credential, handleCredentialChange }}>
             {props.children}
         </CredentialContext.Provider>
     )
