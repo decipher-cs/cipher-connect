89473bc7e71a8b7ddfbac27143defe8eef72959a
foobar <uagnl25x@anonaddy.me>
2023-05-28 16:59:08 +0530

    Changed userList for networkList

--- frontend/src/socket.ts
+++ frontend/src/socket.ts
@@ -4,14 +4,14 @@ import { Message } from './pages/Chat'
 interface ServerToClientEvents {
     noArg: () => void
     withAck: (d: string, callback: (e: number) => void) => void
-    updateUserList: (users: string[]) => void
+    updateNetworkList: (users: string[]) => void
     privateMessage: (target: string, msg: Message) => void
 }
 
 // for socket.on()
 interface ClientToServerEvents {
     privateMessage: (target: string, msg: Message) => void
-    updateUserList: (users: string[]) => void
+    updateNetworkList: (users: string[]) => void
 }
 
 const URL = import.meta.env.PROD ? window.location.origin : import.meta.env.VITE_SERVER_DEV_URL
