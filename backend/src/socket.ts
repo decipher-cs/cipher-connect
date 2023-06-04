1ac46971776b2bd60bb281019b92cc431f3e0e59
foobar <uagnl25x@anonaddy.me>
2023-06-04 19:28:40 +0530

    Removed all functions for network

--- backend/src/socket.ts
+++ backend/src/socket.ts
@@ -6,8 +6,7 @@ interface ServerToClientEvents {
     noArg: () => void
     basicEmit: (a: number, b: string, c: Buffer) => void
     withAck: (d: string, callback: (e: number) => void) => void
-    updateNetworkList: (users: string[]) => void
-    privateMessage: (target: string, msg: string) => void
+    privateMessage: (msg: string) => void
     userRoomsUpdated: (rooms: room[]) => void
     roomChanged: (roomId: string) => void
 }
@@ -17,9 +16,7 @@ interface InterServerEvents {}
 
 // for socket.on()
 interface ClientToServerEvents {
-    privateMessage: (target: string, msg: string) => void
-    updateNetworkList: (users: string[]) => void
-    removeUserFromNetwork: (newConnectionName: string) => void // might wanna use acknowledgment here
+    privateMessage: (msg: string) => void
     addUsersToRoom: (usersToAdd: string[], roomName: string) => void
     createNewRoom: (participant: string, callback: (response: null | string) => void) => void
     roomSelected: (roomId: string) => void
@@ -46,15 +43,7 @@ export const initSocketIO = (io: Server<ClientToServerEvents, ServerToClientEven
 
         const username = socket.data.username
 
-        // const userNetworkList: string[] = []
-
         const userRooms: Awaited<ReturnType<typeof getUserRoomsFromDB>> = []
-
-        // Get list of network from the DB
-        // getUserNetworkList(username).then(networkList => {
-        //     userNetworkList.push(...networkList)
-        //     socket.emit('updateNetworkList', userNetworkList)
-        // })
 
         getUserRoomsFromDB(username).then(rooms => {
             if (rooms === undefined) return
@@ -64,25 +53,11 @@ export const initSocketIO = (io: Server<ClientToServerEvents, ServerToClientEven
 
         socket.join(username)
 
-        socket.on('privateMessage', (target: string, msg: string) => {
+        socket.on('privateMessage', (msg: string) => {
             if (target !== socket.id) {
                 socket.to(target).emit('privateMessage', target, msg)
             } else throw new Error('Message cannot be sent to self')
         })
-
-        // socket.on('updateNetworkList', () => {
-        //     socket.emit('updateNetworkList', userNetworkList)
-        // })
-
-        // socket.on('removeUserFromNetwork', (connectionName: string) => {
-        //     if (userNetworkList.includes(connectionName) === true)
-        //         removeConnectionFromNetwork(username, [connectionName])
-        //             .then(_ => {
-        //                 const index = userNetworkList.indexOf(connectionName)
-        //                 if (index !== -1) userNetworkList.splice(index)
-        //             })
-        //             .catch(err => console.log('while deleting a user from the network list:', err))
-        // })
 
         socket.on('createNewRoom', async (participant, callback) => {
             // check if participant exists
