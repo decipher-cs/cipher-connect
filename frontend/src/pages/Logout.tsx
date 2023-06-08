2ec182622c866592f060362a4e8b580901750988
foobar <uagnl25x@anonaddy.me>
2023-06-08 18:58:06 +0530

    Logout user

---
+++ frontend/src/pages/Logout.tsx
@@ -0,0 +1,26 @@
+import { useContext, useEffect } from 'react'
+import { Navigate } from 'react-router-dom'
+import { CredentialContext } from '../contexts/Credentials'
+
+export const Logout = () => {
+    const { username, isLoggedIn, logUserOut } = useContext(CredentialContext)
+
+    const URL = import.meta.env.PROD ? import.meta.env.VITE_SERVER_PROD_URL : import.meta.env.VITE_SERVER_DEV_URL
+    logUserOut()
+
+    useEffect(() => {
+        if (isLoggedIn === true) {
+            fetch(`${URL}/logout`, {
+                body: JSON.stringify({ username }),
+                method: 'POST',
+                credentials: 'include',
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                },
+            }).then(data => console.log('data iw:', data))
+        }
+    }, [])
+
+    return <Navigate to='/login' replace />
+}
