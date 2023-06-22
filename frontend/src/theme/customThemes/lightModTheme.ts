67d368b0786e8bd5e9ff3ab6ad1f155cc32456e0
foobar <uagnl25x@anonaddy.me>
2023-06-22 22:34:21 +0530

    Added folder for custom theming and custom components

---
+++ frontend/src/theme/customThemes/lightModTheme.ts
@@ -0,0 +1,10 @@
+import { pink, red } from '@mui/material/colors'
+import { createTheme } from '@mui/material'
+
+export const lightMod = createTheme({
+    palette: {
+        primary: {
+            main: red[400],
+        },
+    },
+})
