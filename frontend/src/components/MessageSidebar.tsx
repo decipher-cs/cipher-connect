2d4f9d199669dcf693e9c2a643481ad2d0a32a67
foobar <uagnl25x@anonaddy.me>
2023-07-23 07:07:38 +0530

    added display names for sidebar content

--- frontend/src/components/MessageSidebar.tsx
+++ frontend/src/components/MessageSidebar.tsx
@@ -1,22 +1,11 @@
 import { Avatar, Box, List, ListItem, ListItemIcon, ListItemText } from '@mui/material'
+import { room } from '../types/prisma.client'
+import { RoomWithParticipants } from '../pages/Chat'
+import { useContext } from 'react'
+import { CredentialContext } from '../contexts/Credentials'
 
-export const MessageSidebar = (props: {}) => {
-    return (
-        <Box
-            sx={{
-                border: 'solid red 3px',
-                maxWidth: '30%',
-            }}
-        >
-            <List>
-                {Array(2)
-                    .fill('')
-                    .map(() => (
-                        <MessageListItem />
-                    ))}
-            </List>
-        </Box>
-    )
+interface MessageListItemProps {
+    rooms: RoomWithParticipants[]
 }
 
 const MessageListItem = () => {
@@ -29,5 +18,41 @@ const MessageListItem = () => {
                 <ListItemText primary={<>name</>} secondary={<>subtext</>} />
             </ListItem>
         </>
+    )
+}
+
+interface MessageSidebarProps {
+    rooms: RoomWithParticipants[]
+}
+
+export const MessageSidebar = (props: MessageSidebarProps) => {
+    const { username } = useContext(CredentialContext)
+    return (
+        <Box
+            sx={{
+                border: 'solid red 3px',
+                maxWidth: '30%',
+            }}
+        >
+            <List>
+                {props.rooms.map((room, i) => {
+                    // <MessageListItem key={i} />
+                    let displayName
+                    if (room.isMaxCapacityTwo === true) {
+                        displayName = room.participants.find(({ username: participantUsername }) => participantUsername !== username)?.username
+                    } else {
+                        displayName = room.roomDisplayName
+                    }
+                    return (
+                            <ListItem key={i}>
+                                <ListItemIcon>
+                                    <Avatar />
+                                </ListItemIcon>
+                                <ListItemText primary={<>{displayName}</>} secondary={<>subtext</>} />
+                            </ListItem>
+                    )
+                })}
+            </List>
+        </Box>
     )
 }
