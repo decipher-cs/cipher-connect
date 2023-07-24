f82e1672a02dba2ff29d7bcbf9f49009a17b1a22
foobar <uagnl25x@anonaddy.me>
2023-07-24 23:42:03 +0530

    added type for room with participants

--- frontend/src/types/socket.ts
+++ frontend/src/types/socket.ts
@@ -1,12 +1,13 @@
-import { message as Message, room as Room } from './prisma.client'
+import { Socket } from 'socket.io-client'
+import { message as Message, room as Room, userRoomParticipation as UserRoomParticipation } from './prisma.client'
 
 export interface ServerToClientEvents {
     noArg: () => void
     withAck: (d: string, callback: (e: number) => void) => void
     updateNetworkList: (users: string[]) => void
     privateMessage: (targetRoomId: string, msg: string, senderUsername: string) => void
-    userRoomsUpdated: (rooms: Room[]) => void
-    roomChanged: (room: Room) => void
+    userRoomsUpdated: (rooms: RoomWithParticipants[]) => void
+    roomChanged: (room: RoomWithParticipants) => void
     sendingMessages: () => void
     messagesRequested: (messages: Message[]) => void
 }
@@ -15,11 +16,15 @@ export interface ServerToClientEvents {
 export interface ClientToServerEvents {
     privateMessage: (targetRoomId: string, msg: string) => void
     updateNetworkList: (users: string[]) => void
-    removeUserFromNetwork: (newConnectionName: string) => void // might wanna use acknowledgment here
+    removeUserFromNetwork: (newConnectionName: string) => void
     roomSelected: (roomId: string) => void
-    createNewPrivateRoom: (participant: string, callback: (response: null | string) => void) => void
-    createNewGroup: (participants: string[], displayName: string, callback: (response: null | string) => void) => void
+    createNewPrivateRoom: (participant: string, callback: (response: string) => void) => void
+    createNewGroup: (participants: string[], displayName: string, callback: (response: string) => void) => void
     addUsersToRoom: (usersToAdd: string[], roomName: string) => void
     messagesRequested: (roomId: string) => void
     leaveRoom: (roomId: string) => void
 }
+
+export type SocketWithCustomEvents = Socket<ServerToClientEvents, ClientToServerEvents>
+export type Participants = Pick<UserRoomParticipation, 'username'>[]
+export type RoomWithParticipants = Room & { participants: Participants }
