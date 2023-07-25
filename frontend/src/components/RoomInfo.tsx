a41f500833837565069ca8cb46fcdee466f71c1e
foobar <uagnl25x@anonaddy.me>
2023-07-25 22:48:57 +0530

    added participants and other info

--- frontend/src/components/RoomInfo.tsx
+++ frontend/src/components/RoomInfo.tsx
@@ -1,13 +1,21 @@
-import { Box, Icon } from '@mui/material'
-import React from 'react'
+import { Avatar,TextField, Box, Icon, Typography, IconButton } from '@mui/material'
+import AddIcon from '@mui/icons-material/Add'
 import { RoomWithParticipants } from '../types/socket'
+import { useState } from 'react'
 
 interface RoomInfoProps {
     selectedRoom: RoomWithParticipants | undefined
+    socketObject: SocketWithCustomEvents
 }
 
 export const RoomInfo = (props: RoomInfoProps) => {
+
+    const [contactFieldValue, setContactFieldValue] = useState('')
+
+    const [contactFieldHelperText, setContactFieldHelperText] = useState('')
+
     if (props.selectedRoom === undefined) return <>Select a room</>
+
     return (
         <Box
             sx={{
@@ -16,7 +24,32 @@ export const RoomInfo = (props: RoomInfoProps) => {
             }}
         >
             {/* Group Image */}
-            <Icon />
+            <Avatar/>
+            <Typography variant='h6'>
+                {props.selectedRoom.roomDisplayName}
+            </Typography>
+                {props.selectedRoom.participants.length} members
+            <IconButton>
+                <AddIcon />
+            </IconButton>
+
+            <TextField
+                onChange={e => {
+                    setContactFieldValue(e.target.value)
+                    if (contactFieldHelperText !== '') setContactFieldHelperText('')
+                }}
+                onKeyDown={e => {
+                    if (e.key === 'Enter')
+                        props.socketObject.emit('createNewPrivateRoom', contactFieldValue, response => {
+                            console.log(response)
+                            setContactFieldHelperText(response)
+                        })
+                }}
+                value={contactFieldValue}
+                helperText={contactFieldHelperText}
+                placeholder='Add contact'
+            />
+            { props.selectedRoom.participants.map(({username})=>(<div>{username}</div>)) }
         </Box>
     )
 }
