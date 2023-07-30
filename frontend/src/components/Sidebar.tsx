4e9111755876823968d194ca8b2ec131c7d761ac
foobar <uagnl25x@anonaddy.me>
2023-07-30 17:25:22 +0530

    new component for sidebar

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -1,118 +1,20 @@
-import Box from '@mui/material/Box'
-import Drawer from '@mui/material/Drawer'
-import List from '@mui/material/List'
-import MenuRoundedIcon from '@mui/icons-material/MenuRounded'
-import ListItem from '@mui/material/ListItem'
-import ListItemButton from '@mui/material/ListItemButton'
-import ListItemIcon from '@mui/material/ListItemIcon'
-import ListItemText from '@mui/material/ListItemText'
-import React, { useState } from 'react'
-import { Collapse, IconButton, Tooltip } from '@mui/material'
-import { AccountCircleSharp, DeleteSharp } from '@mui/icons-material'
-import { room as Room } from '../types/prisma.client'
-import VisibilityOff from '@mui/icons-material/VisibilityOff'
+import { Avatar, Box, Button, ButtonGroup, SxProps } from '@mui/material'
 
-interface SidebarProps extends React.PropsWithChildren {
-    listItems: Room[]
-
-    handleClickOnList: (key: string) => void
-    handleClickOnListDeleteIcon: (clickedItem: string) => void
-    handleClickOnItemHideIcon: (clickedItem: string) => void
+interface SidebarProps {
+    sx?: SxProps | undefined
 }
 
 const Sidebar = (props: SidebarProps) => {
-    const [drawerIsOpen, setDrawerIsOpen] = useState(false)
-
-    const [open, setOpen] = useState(true)
-
-    const handleClick = () => {
-        setOpen(!open)
-    }
-
-    const toggleDrawer = (open: boolean) => (event: React.KeyboardEvent | React.MouseEvent) => {
-        if (
-            event.type === 'keydown' &&
-            ((event as React.KeyboardEvent).key === 'Tab' || (event as React.KeyboardEvent).key === 'Shift')
-        ) {
-            return
-        }
-
-        setDrawerIsOpen(open)
-    }
-
-    const SidebarList = () => {
-        return (
-            <Box sx={{ width: 'auto' }} role='presentation'>
-                <List>
-                    {props.listItems.map(({ roomId, roomDisplayName, isMaxCapacityTwo }, i) => (
-                        <ListItem key={i} disablePadding onClick={toggleDrawer(false)}>
-                            <ListItemButton>
-                                <ListItemIcon
-                                    onClick={() => {
-                                        props.handleClickOnList(roomId)
-                                    }}
-                                    onKeyDown={toggleDrawer(false)}
-                                >
-                                    <AccountCircleSharp />
-                                </ListItemIcon>
-                                <ListItemText
-                                    primary={roomDisplayName}
-                                    onClick={() => {
-                                        props.handleClickOnList(roomId)
-                                    }}
-                                    onKeyDown={toggleDrawer(false)}
-                                />
-                            </ListItemButton>
-                            <IconButton
-                                onClick={() => {
-                                    props.handleClickOnItemHideIcon(roomId)
-                                }}
-                                onKeyDown={toggleDrawer(false)}
-                            >
-                                <VisibilityOff />
-                            </IconButton>
-                            <IconButton
-                                onClick={() => {
-                                    // props.handleClickOnListDeleteIcon(roomId)
-                                    console.log('Functionality Temporarily Disabled')
-                                }}
-                                onKeyDown={toggleDrawer(false)}
-                            >
-                                <DeleteSharp />
-                            </IconButton>
-                        </ListItem>
-                        // collapsable list here with all hidden users.
-                        //
-                    ))}
-                </List>
-                <List>
-                    <IconButton
-                        onClick={() => {
-                            handleClick()
-                        }}
-                    >
-                        <VisibilityOff />
-                    </IconButton>
-                    <Collapse in={open} timeout='auto' unmountOnExit>
-                        <ListItem>sample_Item</ListItem>
-                        <ListItem>sample_Item</ListItem>
-                        <ListItem>sample_Item</ListItem>
-                    </Collapse>
-                </List>
-            </Box>
-        )
-    }
     return (
-        <>
-            <Tooltip title='Change Room' placement='left'>
-                <IconButton onClick={toggleDrawer(true)}>
-                    <MenuRoundedIcon />
-                </IconButton>
-            </Tooltip>
-            <Drawer anchor='left' open={drawerIsOpen} onClose={toggleDrawer(false)}>
-                <SidebarList />
-            </Drawer>
-        </>
+        <Box sx={{ ...props.sx, display: 'grid' }}>
+            <Avatar sx={{ justifySelf: 'center' }} />
+            <ButtonGroup orientation='vertical'>
+                <Button>Chat</Button>
+                <Button>Profile Settings</Button>
+                <Button>Logout</Button>
+                <Button>Toggle Theme</Button>
+            </ButtonGroup>
+        </Box>
     )
 }
 
