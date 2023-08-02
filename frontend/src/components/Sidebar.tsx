4141dfe8c75ea28bed08f0e8b1f3d493c5b6bbbd
foobar <uagnl25x@anonaddy.me>
2023-08-02 22:39:59 +0530

    passed settings to child from parent

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -1,17 +1,36 @@
 import { Avatar, Box, Button, ButtonGroup, SxProps } from '@mui/material'
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { Settings, SocketWithCustomEvents } from '../types/socket'
+import { ProfileSettingsDialog } from './ProfileSettingsDialog'
 
 interface SidebarProps {
     sx?: SxProps | undefined
+    socketObject: SocketWithCustomEvents
+    userSettings: Settings
 }
 
 const Sidebar = (props: SidebarProps) => {
+    const navigate = useNavigate()
+
+    const [dialogOpen, setDialogOpen] = useState(true)
+
     return (
         <Box sx={{ ...props.sx, display: 'grid' }}>
-            <Avatar sx={{ justifySelf: 'center' }} />
+            <Avatar sx={{ justifySelf: 'center' }} src='https://www.theventuretours.com/wp-content/uploads/2020/03/avatar-icon-png-1.png'/>
+
+
+            <ProfileSettingsDialog dialogOpen={dialogOpen} setDialogOpen={setDialogOpen} socketObject={props.socketObject} userSettings={props.userSettings}/>
+
             <ButtonGroup orientation='vertical'>
-                <Button>Chat</Button>
-                <Button>Profile Settings</Button>
-                <Button>Logout</Button>
+                <Button onClick={() => navigate('/chat')}>Chat</Button>
+
+                <Button onClick={() => setDialogOpen(true)}>Profile Settings</Button>
+
+                <Button onClick={() => navigate('/logout')}>Logout</Button>
+
+                <Button onClick={() => navigate('/about')}>About</Button>
+
                 <Button>Toggle Theme</Button>
             </ButtonGroup>
         </Box>
