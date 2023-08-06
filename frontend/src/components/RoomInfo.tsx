355bb7f761f554104ea56b997f010303f2dbfd67
foobar <uagnl25x@anonaddy.me>
2023-08-06 15:38:06 +0530

    Removed TextField for private rooms Added source for room avatar

--- frontend/src/components/RoomInfo.tsx
+++ frontend/src/components/RoomInfo.tsx
@@ -2,6 +2,7 @@ import { Avatar, TextField, Box, Icon, Typography, IconButton, Button } from '@m
 import AddIcon from '@mui/icons-material/Add'
 import { RoomWithParticipants, SocketWithCustomEvents } from '../types/socket'
 import { useState } from 'react'
+import { imageBufferToURLOrEmptyString } from '../pages/Chat'
 
 interface RoomInfoProps {
     rooms: RoomWithParticipants[]
@@ -16,7 +17,14 @@ export const RoomInfo = (props: RoomInfoProps) => {
 
     if (props.selectedRoomIndex === undefined) return <>Select a room</>
 
+    const room = props.rooms[props.selectedRoomIndex]
+
+    const roomType = room.isMaxCapacityTwo === true ? 'private' : 'group'
+
+    const roomAvatar = imageBufferToURLOrEmptyString(room.roomDisplayImage)
+
     return (
+        // TODO: Make this a drawer or slider to toggle it.
         <Box
             sx={{
                 border: 'solid blue 3px',
@@ -24,33 +32,37 @@ export const RoomInfo = (props: RoomInfoProps) => {
             }}
         >
             {/* Group Image */}
-            <Avatar />
+            <Avatar src={roomAvatar} />
             <Typography variant='h6'>{props.rooms[props.selectedRoomIndex].roomDisplayName}</Typography>
             {props.rooms[props.selectedRoomIndex].participants.length} members
-            <IconButton>
-                <AddIcon />
-            </IconButton>
-            <TextField
-                onChange={e => {
-                    setContactFieldValue(e.target.value)
-                    if (contactFieldHelperText !== '') setContactFieldHelperText('')
-                }}
-                onKeyDown={e => {
-                    if (e.key === 'Enter' && props.selectedRoomIndex !== undefined)
-                        props.socketObject.emit(
-                            'addParticipantsToGroup',
-                            [contactFieldValue],
-                            props.rooms[props.selectedRoomIndex].roomId,
-                            response => {
-                                console.log(response)
-                                setContactFieldHelperText(response)
-                            }
-                        )
-                }}
-                value={contactFieldValue}
-                helperText={contactFieldHelperText}
-                placeholder='Add contact'
-            />
+            {roomType === 'group' && (
+                <>
+                    <IconButton>
+                        <AddIcon />
+                    </IconButton>
+                    <TextField
+                        onChange={e => {
+                            setContactFieldValue(e.target.value)
+                            if (contactFieldHelperText !== '') setContactFieldHelperText('')
+                        }}
+                        onKeyDown={e => {
+                            if (e.key === 'Enter' && props.selectedRoomIndex !== undefined)
+                                props.socketObject.emit(
+                                    'addParticipantsToGroup',
+                                    [contactFieldValue],
+                                    props.rooms[props.selectedRoomIndex].roomId,
+                                    response => {
+                                        console.log(response)
+                                        setContactFieldHelperText(response)
+                                    }
+                                )
+                        }}
+                        value={contactFieldValue}
+                        helperText={contactFieldHelperText}
+                        placeholder='Add contact'
+                    />
+                </>
+            )}
             {props.rooms[props.selectedRoomIndex].participants.map(({ username }, i) => (
                 <div key={i}>{username}</div>
             ))}
