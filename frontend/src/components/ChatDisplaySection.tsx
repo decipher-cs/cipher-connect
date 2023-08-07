3e1d2a22f2da1e5a32fd7212bb9d241c74897a9f
foobar <uagnl25x@anonaddy.me>
2023-08-07 22:29:14 +0530

    - Refactored input bar to the ChatDisplaySection component. Helps in keeping the textField within the display section of chat. - Removed unneeded CSS files and their imports.

--- frontend/src/components/ChatDisplaySection.tsx
+++ frontend/src/components/ChatDisplaySection.tsx
@@ -4,31 +4,18 @@ import { memo, useContext, useEffect, useRef } from 'react'
 import { CredentialContext } from '../contexts/Credentials'
 import { Message } from '../pages/Chat'
 
-interface SingleTextMessageProps {
-    message: Message
-    endRef: React.RefObject<HTMLDivElement> | null
-}
-
-const SingleTextMessage = memo((props: SingleTextMessageProps) => {
-    const { username } = useContext(CredentialContext)
-
-    return (
-        <Paper
-            sx={{
-                width: 'fit-content',
-                p: 1.5,
-                placeSelf: props.message.senderUsername === username ? 'flex-end' : 'flex-start',
-            }}
-            ref={props.endRef}
-        >
-            <Typography>{props.message.content}</Typography>
-        </Paper>
-    )
-})
+import { ArrowRight } from '@mui/icons-material'
+import { IconButton, InputAdornment, TextField } from '@mui/material'
+import React, { useState } from 'react'
+import { generateDummyMessage } from '../pages/Chat'
+import { socket } from '../socket'
 
 export interface ChatDisplaySectionProps {
     chatMessageList: Message[]
     fakeScrollDiv: React.MutableRefObject<HTMLDivElement | null>
+
+    setChatMessageList: React.Dispatch<React.SetStateAction<Message[]>>
+    currRoom: string | undefined
 }
 
 const ChatDisplaySection = (props: ChatDisplaySectionProps) => {
@@ -66,8 +53,74 @@ const ChatDisplaySection = (props: ChatDisplaySectionProps) => {
                 })}
                 <div ref={scrollToBottomRef} style={{ display: 'none' }}></div>
             </Container>
+
+            <ChatInputBar setChatMessageList={props.setChatMessageList} currRoom={props.currRoom} />
         </>
     )
 }
+
+interface ChatInputBarProps {
+    setChatMessageList: React.Dispatch<React.SetStateAction<Message[]>>
+    currRoom: string | undefined
+}
+
+export const ChatInputBar = (props: ChatInputBarProps) => {
+    const [currInputText, setCurrInputText] = useState('')
+
+    const addMessgeToMessageList = () => {
+        const trimmedText = currInputText.slice().trim()
+        if (trimmedText.length <= 0) return
+
+        if (props.currRoom !== undefined) {
+            socket.emit('privateMessage', props.currRoom, trimmedText)
+        } else console.log('No room selected. This should not be possible.')
+
+        setCurrInputText('')
+    }
+
+    return (
+        <>
+            <TextField
+                sx={{ mt: 10 }}
+                InputProps={{
+                    endAdornment: (
+                        <InputAdornment position='end'>
+                            <IconButton onClick={addMessgeToMessageList}>
+                                <ArrowRight />
+                            </IconButton>
+                        </InputAdornment>
+                    ),
+                }}
+                value={currInputText}
+                fullWidth
+                placeholder='Enter Message'
+                onChange={e => setCurrInputText(e.target.value)}
+                onKeyDown={e => (e.key === 'Enter' ? addMessgeToMessageList() : null)}
+            />
+        </>
+    )
+}
+
+interface SingleTextMessageProps {
+    message: Message
+    endRef: React.RefObject<HTMLDivElement> | null
+}
+
+const SingleTextMessage = memo((props: SingleTextMessageProps) => {
+    const { username } = useContext(CredentialContext)
+
+    return (
+        <Paper
+            sx={{
+                width: 'fit-content',
+                p: 1.5,
+                placeSelf: props.message.senderUsername === username ? 'flex-end' : 'flex-start',
+            }}
+            ref={props.endRef}
+        >
+            <Typography>{props.message.content}</Typography>
+        </Paper>
+    )
+})
 
 export default ChatDisplaySection
