04d184e3df36036d3573de051788bc52787cded9
foobar <uagnl25x@anonaddy.me>
2023-08-10 22:48:56 +0530

    new route added for updating room images

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,5 +1,13 @@
 import { Express } from 'express'
-import { createUser, loginUser, logoutUser, renewAccessToken, varifyRefreshToken } from './controllers.js'
+import {
+    createUser,
+    loginUser,
+    logoutUser,
+    renewAccessToken,
+    updateGroupImage,
+    varifyRefreshToken,
+} from './controllers.js'
+import { avatar } from './server.js'
 
 export const initRoutes = (app: Express) => {
     app.all('/login', loginUser)
@@ -7,4 +15,5 @@ export const initRoutes = (app: Express) => {
     app.all('/renewtoken', renewAccessToken)
     app.all('/logout', logoutUser)
     app.all('/varifyRefreshToken', varifyRefreshToken)
+    app.post('/updateGroupImage', avatar, updateGroupImage)
 }
