d82fe7dbb87a2b76270a7d8acf1f3e69bccd3b44
foobar <uagnl25x@anonaddy.me>
2023-08-17 23:37:17 +0530

    Removed redundent CSS files

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './css/index.css'
-import './css/defaults.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 )
