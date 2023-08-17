c1ccdc9e1138b2bb23b1db78b133d64c84ffd120
foobar <uagnl25x@anonaddy.me>
2023-08-18 00:08:45 +0530

    Added CSS for multiple config options inside RoomInfo

--- frontend/src/components/RoomInfo.tsx
+++ frontend/src/components/RoomInfo.tsx
@@ -9,17 +9,28 @@ import {
     Drawer,
     InputAdornment,
     Divider,
+    Stack,
+    Tooltip,
+    Switch,
+    ButtonGroup,
+    List,
+    ListItem,
+    ListItemAvatar,
+    ListItemText,
 } from '@mui/material'
 import AddIcon from '@mui/icons-material/Add'
 import { RoomWithParticipants, SocketWithCustomEvents } from '../types/socket'
 import { ForwardedRef, forwardRef, Ref, useState } from 'react'
 import { imageBufferToURLOrEmptyString } from '../pages/Chat'
-import { ArrowForwardRounded, SearchSharp } from '@mui/icons-material'
+import { ArrowForwardRounded, CancelRounded, InfoRounded, SearchSharp } from '@mui/icons-material'
+import { StyledTextField } from './StyledTextField'
+import { EditableText } from './EditableText'
+import { Balancer } from 'react-wrap-balancer'
 
 interface RoomInfoProps {
     room: RoomWithParticipants
     socketObject: SocketWithCustomEvents
-    style?: any
+    setRoomInfoVisible: React.Dispatch<React.SetStateAction<boolean>>
 }
 
 export const RoomInfo = (props: RoomInfoProps) => {
@@ -34,6 +45,8 @@ export const RoomInfo = (props: RoomInfoProps) => {
     const [roomName, setRoomName] = useState(props.room.roomDisplayName)
 
     const [roomNameHelperText, setRoomNameHelperText] = useState('')
+
+    const [isTextFieldEnabled, setIsTextFieldEnabled] = useState(false)
 
     const handleRoomNameChange = () => {}
     const handleRoomNameSubmit = () => {}
@@ -65,89 +78,141 @@ export const RoomInfo = (props: RoomInfoProps) => {
     return (
         <Box
             sx={{
-                width: 'max-content',
-                maxWidth: '400px',
+                width: 'min(30vw, 450px)',
                 height: '100%',
 
                 display: 'grid',
                 px: 3,
-                justifyItems: 'center',
+                justifyContent: 'center',
+                alignItems: 'center',
+                alignContent: 'flex-start',
 
-                overflow: 'scroll',
+                overflow: 'visible',
             }}
         >
-            <IconButton component='label' sx={{ justifySelf: 'center' }}>
-                <Avatar src={roomAvatar} sx={{ height: 124, width: 124 }} />
+            <Stack direction='row' sx={{ alignItems: 'center' }}>
+                <InfoRounded sx={{ justifySelf: 'flex-start' }} />
+                <Typography sx={{ justifySelf: 'flex-start' }}>Room Info</Typography>
+                <IconButton
+                    onClick={() => props.setRoomInfoVisible(false)}
+                    sx={{ justifySelf: 'flex-end', ml: 'auto' }}
+                >
+                    <CancelRounded />
+                </IconButton>
+            </Stack>
+            <Divider />
+            <Tooltip title='Click to change image'>
+                <IconButton component='label' sx={{ justifySelf: 'center' }}>
+                    <Avatar src={roomAvatar} sx={{ height: 124, width: 124 }} />
 
-                <input
-                    type='file'
-                    accept='image/*'
-                    hidden
-                    onChange={e => {
-                        handleImageUpload(e.target.files)
-                    }}
-                />
-            </IconButton>
-            <TextField
-                value={roomName}
-                variant='standard'
-                label='Room Name'
-                helperText={roomNameHelperText}
-                onChange={e => {
-                    setRoomName(e.target.value)
-                    if (e.target.value.trim() !== props.room.roomDisplayName) {
-                        setRoomNameHelperText('Submit to change room name')
-                    } else setRoomNameHelperText('')
-                }}
-                InputProps={{
-                    endAdornment: (
-                        <InputAdornment position='end'>
-                            <IconButton onClick={() => {}}>
-                                <ArrowForwardRounded />
-                            </IconButton>
-                        </InputAdornment>
-                    ),
-                }}
-            />
-            members ({props.room.participants.length})
-            {roomType === 'group' && (
-                <>
-                    <IconButton>
-                        <AddIcon />
-                    </IconButton>
-                    <TextField
+                    <input
+                        type='file'
+                        accept='image/*'
+                        hidden
                         onChange={e => {
-                            setContactFieldValue(e.target.value)
-                            if (contactFieldHelperText !== '') setContactFieldHelperText('')
+                            handleImageUpload(e.target.files)
                         }}
-                        onKeyDown={e => {
-                            if (e.key === 'Enter')
-                                props.socketObject.emit(
-                                    'addParticipantsToGroup',
-                                    [contactFieldValue],
-                                    props.room.roomId,
-                                    response => {
-                                        setContactFieldHelperText(response)
-                                    }
-                                )
-                        }}
-                        value={contactFieldValue}
-                        helperText={contactFieldHelperText}
-                        placeholder='Add contact'
                     />
-                </>
-            )}
+                </IconButton>
+            </Tooltip>
+            {/* <Box onClick={() => setIsTextFieldEnabled(p => !p)} sx={{ width: 'max-content' }}> */}
+            {/*     {isTextFieldEnabled ? ( */}
+            {/*         <Typography>{roomName}</Typography> */}
+            {/*     ) : ( */}
+            {/*         <StyledTextField */}
+            {/*             size='small' */}
+            {/*             value={roomName} */}
+            {/*             helperText={roomNameHelperText} */}
+            {/*             onChange={e => { */}
+            {/*                 setRoomName(e.target.value) */}
+            {/*                 if (e.target.value.trim() !== props.room.roomDisplayName) { */}
+            {/*                     setRoomNameHelperText('Submit to change room name') */}
+            {/*                 } else setRoomNameHelperText('') */}
+            {/*             }} */}
+            {/*             // InputProps={{ */}
+            {/*             //     endAdornment: ( */}
+            {/*             //         <InputAdornment position='end'> */}
+            {/*             //             <IconButton onClick={() => {}}> */}
+            {/*             //                 <ArrowForwardRounded /> */}
+            {/*             //             </IconButton> */}
+            {/*             //         </InputAdornment> */}
+            {/*             //     ), */}
+            {/*             // }} */}
+            {/*         /> */}
+            {/*     )} */}
+            {/* </Box> */}
+            <EditableText text={roomName} setText={setRoomName} />
+            <Box sx={{ maxHeight: '40%', overflowY: 'auto' }}>
+                <Typography>Description:</Typography>
+                <Typography variant='body2' paragraph>
+                    <Balancer>
+                        Reprehenderit nostrud nostrud ipsum Lorem est aliquip amet voluptate vvoluptate dolor minim null
+                        aoluptate dolor minim nullavoluptate dolor minim null
+                    </Balancer>
+                </Typography>
+            </Box>
+            <Divider />
+            Notifications <Switch />
+            <Divider />
+            <Box>
+                Members ({props.room.participants.length})
+                {roomType === 'group' && (
+                    <>
+                        <IconButton>
+                            <AddIcon />
+                        </IconButton>
+                        <StyledTextField
+                            onChange={e => {
+                                setContactFieldValue(e.target.value)
+                                if (contactFieldHelperText !== '') setContactFieldHelperText('')
+                            }}
+                            onKeyDown={e => {
+                                if (e.key === 'Enter')
+                                    props.socketObject.emit(
+                                        'addParticipantsToGroup',
+                                        [contactFieldValue],
+                                        props.room.roomId,
+                                        response => {
+                                            setContactFieldHelperText(response)
+                                        }
+                                    )
+                            }}
+                            value={contactFieldValue}
+                            helperText={contactFieldHelperText}
+                            placeholder='Add contact'
+                            InputProps={{
+                                endAdornment: (
+                                    <InputAdornment position='end'>
+                                        <IconButton onClick={() => {}}>
+                                            <ArrowForwardRounded />
+                                        </IconButton>
+                                    </InputAdornment>
+                                ),
+                            }}
+                        />
+                    </>
+                )}
+            </Box>
             <Box>
                 <Divider />
-                {props.room.participants.map(({ username }, i) => (
-                    <div key={i}>{username}</div>
-                ))}
-                <Divider />
+                <List>
+                    {props.room.participants.map(({ username }, i) => (
+                        <ListItem key={i}>
+                            <ListItemAvatar>
+                                <Avatar src='' />
+                            </ListItemAvatar>
+                            <ListItemText>{username}</ListItemText>
+                        </ListItem>
+                    ))}
+                </List>
             </Box>
             {props.room.isMaxCapacityTwo === false ? (
                 <>
-                    <Button onClick={() => {}}>Delete Group</Button>
-                    <Button onClick={() => {}}>Leave Group</Button>
+                    <Divider />
+                    <ButtonGroup size='small' variant='text'>
+                        <Button onClick={() => {}}>Delete Group</Button>
+                        <Button onClick={() => {}}>Leave Group</Button>
+                    </ButtonGroup>
                 </>
             ) : null}
         </Box>
