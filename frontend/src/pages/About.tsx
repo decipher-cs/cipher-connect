51f4c8f067b4f5a42205447316f65adf9eceeb96
foobar <uagnl25x@anonaddy.me>
2023-08-19 00:29:38 +0530

    Added Balancer to the text with temporary CSS

--- frontend/src/pages/About.tsx
+++ frontend/src/pages/About.tsx
@@ -1,19 +1,22 @@
-import { Box, Link, Typography } from '@mui/material'
+import { Box, Container, Link, Typography } from '@mui/material'
 import { useNavigate } from 'react-router-dom'
+import { Balancer } from 'react-wrap-balancer'
 
 export const About = (props: {}) => {
     const navigate = useNavigate()
     return (
-        <Box>
-            <Typography>
-                This is a demo chat app made by <Link href='https://github.com/decipher-cs/'>me</Link>. Design was
-                inspired by
-                <Link href='https://dribbble.com/shots/18945888-Messenger-Mobile-Web-Application-Light-Mode/attachments/14116324?mode=media'>
-                    Mohammad Hashemi
-                </Link>
-                on dribble.
-            </Typography>
+        <Container>
+            <Balancer>
+                <Typography variant='h3'>
+                    This is a demo chat app made by <Link href='https://github.com/decipher-cs/'>decipher</Link>. Design
+                    was inspired by{' '}
+                    <Link href='https://dribbble.com/shots/18945888-Messenger-Mobile-Web-Application-Light-Mode/attachments/14116324?mode=media'>
+                        Mohammad Hashemi
+                    </Link>{' '}
+                    on dribble.
+                </Typography>
+            </Balancer>
             <Link onClick={() => navigate('/chat')}>Go Back to chat</Link>
-        </Box>
+        </Container>
     )
 }
