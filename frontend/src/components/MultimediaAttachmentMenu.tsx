e7b2b51c0f06aba68361bcb35d1148a8f70e9f84
foobar <uagnl25x@anonaddy.me>
2023-08-27 02:09:00 +0530

    Menu now uses input element to take audio, video, and image files from user

--- frontend/src/components/MultimediaAttachmentMenu.tsx
+++ frontend/src/components/MultimediaAttachmentMenu.tsx
@@ -1,20 +1,19 @@
-import { FilePresentRounded, ImageRounded, VideoCameraBackRounded } from '@mui/icons-material'
-import { IconButton, Menu, MenuItem } from '@mui/material'
-import React from 'react'
+import { AudiotrackRounded, FilePresentRounded, ImageRounded, VideoCameraBackRounded } from '@mui/icons-material'
+import { Button, Input, InputBase, ListItemIcon, ListItemText, Menu, MenuItem, MenuList } from '@mui/material'
+import React, { useState } from 'react'
+import { MessageContentType } from '../types/prisma.client'
 
 export const MultimediaAttachmentMenu = (props: {
     anchorEl: HTMLElement | null
     setAnchorEl: React.Dispatch<React.SetStateAction<HTMLElement | null>>
+    handleUpload: (fileList: FileList | null, type: MessageContentType) => void
 }) => {
     const open = Boolean(props.anchorEl)
 
-    const handleClose = () => {
-        props.setAnchorEl(null)
-    }
     return (
         <Menu
             open={open}
-            onClose={handleClose}
+            onClose={() => props.setAnchorEl(null)}
             anchorEl={props.anchorEl}
             anchorOrigin={{
                 vertical: 'top',
@@ -26,21 +25,58 @@ export const MultimediaAttachmentMenu = (props: {
             }}
             variant='menu'
         >
-            <MenuItem onClick={handleClose}>
-                <IconButton>
-                    <VideoCameraBackRounded />
-                </IconButton>
-            </MenuItem>
-            <MenuItem onClick={handleClose}>
-                <IconButton>
-                    <ImageRounded />
-                </IconButton>
-            </MenuItem>
-            <MenuItem onClick={handleClose}>
-                <IconButton>
-                    <FilePresentRounded />
-                </IconButton>
-            </MenuItem>
+            <MenuList>
+                <MenuItem component='label'>
+                    <input
+                        multiple
+                        type='file'
+                        accept='audio/*'
+                        hidden
+                        onChange={e => props.handleUpload(e.target.files, 'audio')}
+                    />
+                    <ListItemIcon>
+                        <AudiotrackRounded />
+                    </ListItemIcon>
+                    <ListItemText>Audio</ListItemText>
+                </MenuItem>
+
+                <MenuItem component='label'>
+                    <input
+                        multiple
+                        type='file'
+                        accept='image/*'
+                        hidden
+                        onChange={e => props.handleUpload(e.target.files, 'image')}
+                    />
+                    <ListItemIcon>
+                        <ImageRounded />
+                    </ListItemIcon>
+                    <ListItemText>Image</ListItemText>
+                </MenuItem>
+
+                <MenuItem component='label'>
+                    <input
+                        multiple
+                        type='file'
+                        accept='video/*'
+                        hidden
+                        onChange={e => props.handleUpload(e.target.files, 'video')}
+                    />
+                    <ListItemIcon>
+                        <VideoCameraBackRounded />
+                    </ListItemIcon>
+                    <ListItemText>Video</ListItemText>
+                </MenuItem>
+
+                {/* TODO: implement type 'application/*' */}
+                {/* <MenuItem> */}
+                {/*     <input type='file' accept='' hidden onChange={e => handleClose(e.target.files)} /> */}
+                {/*     <ListItemIcon> */}
+                {/*         <FilePresentRounded /> */}
+                {/*     </ListItemIcon> */}
+                {/*     <ListItemText>Add File</ListItemText> */}
+                {/* </MenuItem> */}
+            </MenuList>
         </Menu>
     )
 }
