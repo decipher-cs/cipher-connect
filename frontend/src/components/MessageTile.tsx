606d8ef1a5158caab27f82206f0a44299240a86e
foobar <uagnl25x@anonaddy.me>
2023-08-27 23:36:04 +0530

    Added CSS for multimedia files so they dont overflow and fit the container

--- frontend/src/components/MessageTile.tsx
+++ frontend/src/components/MessageTile.tsx
@@ -1,4 +1,5 @@
-import { Paper, Typography } from '@mui/material'
+import { Dialog, Paper, Typography } from '@mui/material'
+import { useState } from 'react'
 import { MessageContentType } from '../types/prisma.client'
 
 export const MessageTile = (props: {
@@ -7,17 +8,34 @@ export const MessageTile = (props: {
     alignment: 'left' | 'right'
     autoScrollToBottomRef: React.RefObject<HTMLDivElement> | null
 }) => {
+    const [showDialog, setShowDialog] = useState(false)
+
     const Message = () => {
         if (props.messageContentType === 'text') {
             return <Typography sx={{ overflowWrap: 'break-word', color: 'white' }}>{props.content}</Typography>
         } else if (props.messageContentType === 'audio') {
             return <audio controls src={props.content} />
-        } else if (props.messageContentType === 'image') {
-            return <img src={props.content} loading='lazy' width='50%' />
         } else if (props.messageContentType === 'video') {
-            return <video src={props.content} controls />
+            return <video src={props.content} controls style={{ maxHeight: '30svh' }} />
+        } else if (props.messageContentType === 'image') {
+            return (
+                <>
+                    <img
+                        src={props.content}
+                        loading='lazy'
+                        style={{
+                            maxHeight: '30svh',
+                            maxWidth: '100%',
+                        }}
+                        onClick={() => setShowDialog(true)}
+                    />
+                    <Dialog open={showDialog} onClick={() => setShowDialog(false)}>
+                        <img src={props.content} />
+                    </Dialog>
+                </>
+            )
         }
-        return <div />
+        return null
     }
 
     return (
@@ -26,7 +44,7 @@ export const MessageTile = (props: {
                 borderRadius: props.alignment === 'left' ? '0px 45px 45px 45px' : '45px 0px 45px 45px',
                 px: 4,
                 py: 3,
-                alignSelf: props.alignment === 'left' ? 'flex-start' : 'flex-end',
+                justifySelf: props.alignment === 'left' ? 'flex-start' : 'flex-end',
                 width: 'fit-content',
                 maxWidth: '80%',
                 backgroundImage: 'linear-gradient(45deg,#3023AE 0%,#FF0099 100%)',
