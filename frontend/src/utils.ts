847ef9c93ae935cbe1bb5d9184c4830e1e4dda65
foobar <uagnl25x@anonaddy.me>
2023-09-04 23:35:46 +0530

    Removed templateMessage funtion and added MIME support to arraybufferToObjectURL function

--- frontend/src/utils.ts
+++ frontend/src/utils.ts
@@ -1,27 +1,6 @@
-import { MessageContentType, MessageToServer } from './types/prisma.client'
-
-export const arrayBufferToObjectUrlConverter = (buffer: ArrayBuffer | null) => {
+export const arrayBufferToObjectUrlConverter = (buffer: ArrayBuffer | null, MIMEType: string) => {
     if (buffer === null) return ''
-    const file = new File([buffer], 'file_')
+    const [fileType, fileExtention] = MIMEType.split('/')
+    const file = new File([buffer], crypto.randomUUID(), { type: fileExtention })
     return URL.createObjectURL(file)
-}
-
-export const messageTemplate = (
-    targetRoomId: string,
-    messageContents: File | Blob | string,
-    senderUsername: string,
-    messageType: MessageContentType,
-    key?: string,
-    editedAt?: Date,
-    createdAt?: Date
-): MessageToServer => {
-    return {
-        key: key ?? crypto.randomUUID(),
-        editedAt: editedAt ?? null,
-        senderUsername,
-        roomId: targetRoomId,
-        contentType: messageType,
-        createdAt: createdAt ?? new Date(),
-        content: messageContents,
-    }
 }
