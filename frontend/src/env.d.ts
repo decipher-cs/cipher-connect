ee6ea4ab43a08f3e2425db3558ab158e5b80f938
foobar <uagnl25x@anonaddy.me>
2023-09-14 22:56:20 +0530

    Removed redundent variables

--- frontend/src/env.d.ts
+++ frontend/src/env.d.ts
@@ -2,10 +2,7 @@
 
 interface ImportMetaEnv {
     readonly VITE_APP_TITLE: string
-    readonly VITE_SERVER_PROD_URL: string
-    readonly VITE_SERVER_DEV_URL: string
-    readonly VITE_CLIENT_PROD_URL: string
-    readonly VITE_CLIENT_DEV_URL: string
+    readonly VITE_SERVER_URL: string
 }
 
 interface ImportMeta {
