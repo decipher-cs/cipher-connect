e718b4721bcba8d850e831ad070f4f4d7dab3735
foobar <uagnl25x@anonaddy.me>
2023-09-14 20:38:54 +0530

    Types for roues adding type safety on client-side code

---
+++ frontend/src/types/routes.ts
@@ -0,0 +1,12 @@
+export const Routes = {
+    login: '/login',
+    signup: '/signup',
+    renewtoken: '/renewtoken',
+    logout: '/logout',
+    varifyRefreshToken: '/varifyRefreshToken',
+    updateGroupImage: '/updateGroupImage',
+    userSettings: '/userSettings',
+    userRoomsAndParticipants: '/userRoomsAndParticipants',
+    roomParticipants: '/roomParticipants',
+    messages: '/Messages',
+} as const
