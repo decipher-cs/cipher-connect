47c54c266ab7384d1998786d99b90badb96e5560
foobar <uagnl25x@anonaddy.me>
2023-09-22 11:19:17 +0530

    Get and post routes separated

--- frontend/src/types/routes.ts
+++ frontend/src/types/routes.ts
@@ -1,14 +1,19 @@
 export const Routes = {
+    get: {
+        messages: '/Messages',
+        roomParticipants: '/roomParticipants',
+        userRooms: '/user-rooms',
+        users: '/users',
+        user: '/user',
+    },
+    post: {
+        avatar: '/avatar',
+        media: '/Media',
+    },
+
     login: '/login',
     signup: '/signup',
     renewtoken: '/renewtoken',
     logout: '/logout',
-    varifyRefreshToken: '/varifyRefreshToken',
-    updateAvatar: '/updateAvatar',
-    userSettings: '/userSettings',
-    userRoomsAndParticipants: '/userRoomsAndParticipants',
-    roomParticipants: '/roomParticipants',
-    messages: '/Messages',
-    media: '/Media',
-    avatar: '/avatar',
+    varifyRefreshToken: '/varify-refresh-token',
 } as const
