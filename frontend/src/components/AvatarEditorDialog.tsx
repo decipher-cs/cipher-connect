637114a75b5e9aabd6e93c839cfbbdb13ce791a0
foobar <uagnl25x@anonaddy.me>
2023-09-22 11:21:00 +0530

    New component for editing avatars before uploading them

---
+++ frontend/src/components/AvatarEditorDialog.tsx
@@ -0,0 +1,107 @@
+import AvatarEditor from 'react-avatar-editor'
+
+import React, { Ref, useRef, useState } from 'react'
+import {
+    AvatarClassKey,
+    AvatarProps,
+    Box,
+    Button,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogTitle,
+    Slider,
+    Stack,
+    Typography,
+} from '@mui/material'
+
+export const AvatarEditorDialog = (props: {
+    open: boolean
+    imgSrc: File | string
+    editorRef: React.RefObject<AvatarEditor>
+    handleClose: () => void
+}) => {
+    const [config, setConfig] = useState({
+        image: props.imgSrc,
+        rotate: 0,
+        scale: 1,
+        // position: Position,
+        // preview: {
+        //     img: string,
+        //     rect: {
+        //         x: number,
+        //         y: number,
+        //         width: number,
+        //         height: number,
+        //     },
+        //     scale: number,
+        //     width: number,
+        //     height: number,
+        //     borderRadius: number,
+        // },
+        width: 350,
+        height: 350,
+        disableCanvasRotation: true,
+        // isTransparent: true,
+        // showGrid: true, // doesn't work but why?
+        // backgroundColor: ,
+        border: 100,
+        borderRadius: 999,
+    })
+
+    return (
+        <Dialog fullScreen open={props.open}>
+            <DialogTitle>Edit Avatar</DialogTitle>
+            <DialogContent dividers>
+                <Stack spacing={2} direction='row' sx={{ mb: 1 }} alignItems='center'>
+                    <Stack spacing={2} direction='column' sx={{ m: 1, flexGrow: 1 }}>
+                        <Typography>Zoom</Typography>
+                        <Slider
+                            onChange={(_, scale) => Array.isArray(scale) || setConfig(p => ({ ...p, scale }))}
+                            min={0}
+                            max={3.0}
+                            value={config.scale}
+                            step={0.1}
+                            valueLabelDisplay='auto'
+                            marks={[
+                                { value: 0, label: 0 },
+                                { value: 1, label: 1 },
+                                { value: 2, label: 2 },
+                                { value: 3, label: 3 },
+                            ]}
+                        />
+                        <br />
+                        <br />
+                        <br />
+                        <Typography>Rotate</Typography>
+                        <Slider
+                            onChange={(_, rotate) => Array.isArray(rotate) || setConfig(p => ({ ...p, rotate }))}
+                            min={-360}
+                            max={360}
+                            value={config.rotate}
+                            step={10}
+                            valueLabelDisplay='auto'
+                            marks={[
+                                { value: -360, label: -360 },
+                                { value: 0, label: 0 },
+                                { value: 360, label: 360 },
+                            ]}
+                        />
+                    </Stack>
+                    <AvatarEditor {...config} ref={props.editorRef} />
+                </Stack>
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={props.handleClose}>Cancel</Button>
+                <Button
+                    variant='contained'
+                    onClick={() => {
+                        props.handleClose()
+                    }}
+                >
+                    Confirm
+                </Button>
+            </DialogActions>
+        </Dialog>
+    )
+}
