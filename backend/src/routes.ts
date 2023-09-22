2b8ee736137a921993bf1d90d845ff7114235c34
foobar <uagnl25x@anonaddy.me>
2023-09-22 14:23:19 +0530

    New controllers added for: - Storing images - Retrieving images - Getting user info - Getting user related rooms

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -4,36 +4,37 @@ import {
     loginUser,
     logoutUser,
     renewAccessToken,
-    returnUserSettings,
-    updateGroupImage,
+    // returnUserSettings,
     varifyRefreshToken,
     fetchAllRoomPariticpants,
-    returnUserRoomsAndParticipants,
     fetchMessages,
-    returnMediaFromFS,
     storeMediaToFS,
     storeAvatarToFS,
-    returnAvatarFromFS,
+    returnUserRooms,
+    returnUsers,
+    returnUser,
 } from './controllers.js'
 import { avatar, media } from './server.js'
 
 // To be used for type safety on the client side
 const routes = {
-    // get: {
-    //
-    // }
+    get: {
+        messages: '/Messages',
+        roomParticipants: '/roomParticipants',
+        userRooms: '/user-rooms',
+        users: '/users',
+        user: '/user',
+    },
+    post: {
+        avatar: '/avatar',
+        media: '/Media',
+    },
+
     login: '/login',
     signup: '/signup',
     renewtoken: '/renewtoken',
     logout: '/logout',
-    varifyRefreshToken: '/varifyRefreshToken',
-    // updateAvatar: '/updateAvatar',
-    userSettings: '/userSettings',
-    userRoomsAndParticipants: '/userRoomsAndParticipants',
-    roomParticipants: '/roomParticipants',
-    messages: '/Messages',
-    media: '/Media',
-    avatar: '/avatar',
+    varifyRefreshToken: '/varify-refresh-token',
 } as const
 
 export const initRoutes = (app: Express) => {
@@ -43,15 +44,15 @@ export const initRoutes = (app: Express) => {
     app.all(routes.logout, logoutUser)
     app.all(routes.varifyRefreshToken, varifyRefreshToken)
 
-    app.post(routes.avatar, avatar, storeAvatarToFS)
-    app.get(routes.avatar + '/:path', returnAvatarFromFS)
+    app.get(routes.get.userRooms + '/:username', returnUserRooms)
+    app.get(routes.get.users, returnUsers)
+    app.get(routes.get.user + '/:username', returnUser)
 
-    app.get(routes.userSettings + '/:username', returnUserSettings)
+    app.get(routes.get.roomParticipants, fetchAllRoomPariticpants)
 
-    app.get(routes.userRoomsAndParticipants + '/:username', returnUserRoomsAndParticipants)
-    app.get(routes.roomParticipants, fetchAllRoomPariticpants)
+    app.get(routes.get.messages + '/:roomID', fetchMessages)
 
-    app.get(routes.messages + '/:roomID', fetchMessages)
-    app.get(routes.media + '/:path', returnMediaFromFS)
-    app.post(routes.media, media, storeMediaToFS)
+    app.post(routes.post.media, media, storeMediaToFS)
+
+    app.post(routes.post.avatar, avatar, storeAvatarToFS)
 }
