8425ace9766faa9ed4649cae665ae23fe3481a40
foobar <uagnl25x@anonaddy.me>
2023-09-26 23:35:35 +0530

    Imported missing imports for types

--- backend/src/models/delete.ts
+++ backend/src/models/delete.ts
@@ -1,10 +1,9 @@
-import { prisma } from "../server.js"
+import { prisma } from '../server.js'
 
 export const deleteAllUsers = async () => {
     const deletedUsers = await prisma.user.deleteMany()
     return deletedUsers
 }
-
 
 export const deleteRefreshToken = async (username: string) => {
     const removedCount = await prisma.refreshToken.deleteMany({ where: { username } })
--- backend/src/models/update.ts
+++ backend/src/models/update.ts
@@ -1,19 +1,20 @@
-import { prisma } from "../server.js"
+import { Room, User } from '@prisma/client'
+import { prisma } from '../server.js'
 
 export const updateUser = async (username: string, user: Partial<User>) => {
     return await prisma.user.update({ where: { username }, data: { ...user } })
-}
-
-export const updateRoomImage = async (roomId: string, pathToImg: string) => {
-    return await prisma.room.update({
-        where: { roomId },
-        data: { roomDisplayImagePath: pathToImg },
-    })
 }
 
 export const updateRoom = async (roomId: string, room: Partial<Room>) => {
     return await prisma.room.update({
         where: { roomId },
         data: { ...room },
+    })
+}
+
+export const updateRoomImage = async (roomId: string, pathToImg: string) => {
+    return await prisma.room.update({
+        where: { roomId },
+        data: { roomAvatar: pathToImg },
     })
 }
