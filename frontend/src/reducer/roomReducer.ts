c3ab01271818c05cdb69fa2a920f75f427b87f25
foobar <uagnl25x@anonaddy.me>
2023-09-30 23:28:15 +0530

    Changed type of Room to RoomDetails

--- frontend/src/reducer/roomReducer.ts
+++ frontend/src/reducer/roomReducer.ts
@@ -1,36 +1,52 @@
-import { Message, Room } from '../types/prisma.client'
-import { RoomWithParticipants } from '../types/socket'
+import { Room } from '../types/prisma.client'
+import { RoomDetails, RoomWithParticipants } from '../types/prisma.client'
 
-type RoomsState = {
+export type RoomsState = {
     selectedRoom: null | number
-    joinedRooms: RoomWithParticipants[]
+    joinedRooms: RoomDetails[]
 }
 
 export enum RoomActionType {
-    INITIALIZE_ROOM = 'initializeRoom',
-    ADD_ROOM = 'addRoom',
-    HIDE = 'hide',
-    ADD_PARTICIPANT = 'addParticipant',
-    CHANGE_ROOM = 'changeRoom',
+    initializeRoom = 'initializeRoom',
+    addRoom = 'addRoom',
+    hide = 'hide',
+    addParticipants = 'addParticipant',
+    changeRoom = 'changeRoom',
+    alterRoomProperties = 'alterRoomProperties',
+    changeNotificationStatus = 'changeNotificationStatus',
 }
 
 export type RoomActions =
     | {
-          type: RoomActionType.INITIALIZE_ROOM
-          rooms: RoomWithParticipants[]
+          type: RoomActionType.initializeRoom
+          rooms: RoomsState['joinedRooms']
       }
     | {
-          type: RoomActionType.ADD_ROOM
-          room: RoomWithParticipants | RoomWithParticipants[]
+          type: RoomActionType.addRoom
+          room: RoomsState['joinedRooms'][0] | RoomsState['joinedRooms']
       }
     | {
-          type: RoomActionType.CHANGE_ROOM
+          type: RoomActionType.changeRoom
           newRoomIndex: RoomsState['selectedRoom']
       }
     | {
-          type: RoomActionType.ADD_PARTICIPANT
+          type: RoomActionType.addParticipants
           roomId: Room['roomId']
-          participants: RoomWithParticipants['participants']
+          participants: RoomsState['joinedRooms'][0]['participants']
+      }
+    | {
+          type: RoomActionType.alterRoomProperties
+          roomId: Room['roomId']
+
+          //roomId, username, roomtype, and pariticpants should be readonly.
+          newRoomProperties: Partial<
+              Omit<RoomsState['joinedRooms'][0], 'roomId' | 'roomType' | 'username' | 'participants'>
+          >
+      }
+    | {
+          type: RoomActionType.changeNotificationStatus
+          roomId: RoomsState['joinedRooms'][0]['roomId']
+          unreadMessages: boolean
       }
 
 export const roomReducer: React.Reducer<RoomsState, RoomActions> = (state, action) => {
@@ -38,28 +54,45 @@ export const roomReducer: React.Reducer<RoomsState, RoomActions> = (state, actio
 
     const room: RoomsState = structuredClone(state)
 
+    const roomIndex = room.selectedRoom
+    const selectedRoom = roomIndex !== null ? room.joinedRooms[roomIndex] : null
+
     switch (type) {
-        case RoomActionType.INITIALIZE_ROOM:
+        case RoomActionType.initializeRoom:
             room.joinedRooms = action.rooms
             break
-        case RoomActionType.ADD_ROOM:
+
+        case RoomActionType.addRoom:
             if (Array.isArray(action.room)) {
                 action.room.forEach(r => room.joinedRooms.push(r))
             } else room.joinedRooms.push(action.room)
             break
 
-        case RoomActionType.ADD_PARTICIPANT:
+        case RoomActionType.addParticipants:
             // room.joinedRooms.find(r => r.roomId === action.roomId)
             break
 
-        case RoomActionType.CHANGE_ROOM:
+        case RoomActionType.changeRoom:
             // if (action.newRoomIndex === null) room.selectedRoom = undefined!
             room.selectedRoom = action.newRoomIndex
             break
 
+        case RoomActionType.changeNotificationStatus:
+            if (selectedRoom !== null && selectedRoom.roomId === action.roomId) {
+                selectedRoom.hasUnreadMessages = action.unreadMessages
+            }
+            break
+
+        case RoomActionType.alterRoomProperties:
+            if (!room.selectedRoom) break
+            room.joinedRooms[room.selectedRoom] = {
+                ...room.joinedRooms[room.selectedRoom],
+                ...action.newRoomProperties,
+            }
+            break
+
         default:
             throw new Error('Unknown Error')
-            break
     }
     return room
 }
