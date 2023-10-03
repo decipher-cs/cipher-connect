30ec3eb342ce9708a4aff59519df1c052619763f
foobar <uagnl25x@anonaddy.me>
2023-10-04 01:52:09 +0530

    removed utils file

--- frontend/src/utils.ts
+++
@@ -1,6 +0,0 @@
-export const arrayBufferToObjectUrlConverter = (buffer: ArrayBuffer | null, MIMEType: string) => {
-    if (buffer === null) return ''
-    const [fileType, fileExtention] = MIMEType.split('/')
-    const file = new File([buffer], crypto.randomUUID(), { type: fileExtention })
-    return URL.createObjectURL(file)
-}
