fa6fc796f00d87c69a2d5272be9c642ec7060711
foobar <uagnl25x@anonaddy.me>
2023-10-30 05:48:34 +0530

    Removed variables for separate storage of avatars and media

--- frontend/src/env.d.ts
+++ frontend/src/env.d.ts
@@ -3,8 +3,6 @@
 interface ImportMetaEnv {
     readonly VITE_APP_TITLE: string
     readonly VITE_SERVER_URL: string
-    readonly VITE_MEDIA_STORAGE_URL: string
-    readonly VITE_AVATAR_STORAGE_URL: string
 }
 
 interface ImportMeta {
