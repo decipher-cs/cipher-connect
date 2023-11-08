c29c1fe2ec81766ab89abc299d49d68e7765052f
foobar <uagnl25x@anonaddy.me>
2023-11-09 00:18:42 +0530

    Improvement: changed the about page

--- frontend/src/pages/About.tsx
+++ frontend/src/pages/About.tsx
@@ -1,22 +1,36 @@
-import { Box, Container, Link, Typography } from '@mui/material'
+import { Box, Button, ButtonGroup, Container, Link, Typography } from '@mui/material'
 import { useNavigate } from 'react-router-dom'
 import { Balancer } from 'react-wrap-balancer'
 
 export const About = (props: {}) => {
     const navigate = useNavigate()
     return (
-        <Container>
+        <Container
+            sx={{
+                height: '100svh',
+                maxWidth: '100vw',
+                display: 'grid',
+                placeItems: 'flex-start',
+                placeContent: 'flex-start',
+                gap: 5,
+            }}
+        >
             <Balancer>
-                <Typography variant='h3'>
-                    This is a demo chat app made by <Link href='https://github.com/decipher-cs/'>decipher</Link>. Design
-                    was inspired by{' '}
+                <Typography variant='h5'>
+                    This is a demo for a multimedia messaging application made by
+                    <Link href='https://github.com/decipher-cs/'> decipher </Link>. The design was inspired by{' '}
                     <Link href='https://dribbble.com/shots/18945888-Messenger-Mobile-Web-Application-Light-Mode/attachments/14116324?mode=media'>
-                        Mohammad Hashemi
-                    </Link>{' '}
-                    on dribble.
+                        Mohammad Hashemi's{' '}
+                    </Link>
+                    messagenger application on dribble. Please check them out.
                 </Typography>
             </Balancer>
-            <Link onClick={() => navigate('/chat')}>Go Back to chat</Link>
+            <ButtonGroup variant='outlined'>
+                <Button onClick={() => navigate('/chat')}>Go Back to chat</Button>
+                <Button LinkComponent={'button'} href='https://github.com/decipher-cs/'>
+                    Source Code
+                </Button>
+            </ButtonGroup>
         </Container>
     )
 }
