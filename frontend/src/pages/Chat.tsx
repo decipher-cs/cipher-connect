28771c1aa63ec16b0f7318b610ea7626237a056b
foobar <uagnl25x@anonaddy.me>
2023-11-23 01:21:12 +0530

    SX: Added background pattern on chat display

--- frontend/src/pages/Chat.tsx
+++ frontend/src/pages/Chat.tsx
@@ -85,11 +85,23 @@ export const Chat = () => {
                                 flexBasis: '100%',
                                 flexShrink: 1,
                                 display: 'grid',
-                                background: theme =>
-                                    theme.palette.mode === 'dark'
-                                        ? theme.palette.background.default
-                                        : 'linear-gradient(135deg, #f4e9e5,#f4dcd0,#eed0d2,#dbd7e4,#e4e8f2)',
                                 gridTemplateRows: '1fr auto',
+                                background: theme => {
+                                    if (theme.palette.mode === 'light')
+                                        return 'linear-gradient(135deg, #f4e9e5,#f4dcd0,#eed0d2,#dbd7e4,#e4e8f2)'
+                                    const size = '30px'
+                                    const loopColor = theme.palette.background.default
+                                    const loop = `#0000 46%, ${loopColor} 47% 53%, #0000 54%`
+
+                                    return `radial-gradient(100% 100% at 100% 101%, ${loop}) ${size} ${size},
+                                            radial-gradient(100% 100% at 0 0, ${loop}) ${size} ${size},
+                                    radial-gradient(100% 100%, ${theme.palette.background.dark} 22%, ${loopColor} 23% 29%, ${theme.palette.background.dark} 30% 34%, ${loopColor} 35% 41%, #0000 42%) ${theme.palette.background.dark}`
+                                },
+                                backgroundSize: theme => {
+                                    if (theme.palette.mode === 'light') return
+                                    const size = 30
+                                    return `${size * 2}px ${size * 2}px`
+                                },
                             }}
                         >
                             <ChatDisplaySection
