d49828c01ecffd85c40f59ad817fb3e75f29e719
foobar <uagnl25x@anonaddy.me>
2023-11-28 00:14:40 +0530

    FIX: fixed socket.io not working in prod due to broken URL

--- frontend/src/socket.ts
+++ frontend/src/socket.ts
@@ -1,6 +1,6 @@
 import { io, Socket } from 'socket.io-client'
 import { ClientToServerEvents, ServerToClientEvents } from './types/socket'
 
-const URL = import.meta.env.PROD ? window.location.origin : import.meta.env.VITE_SERVER_URL
+const URL = import.meta.env.VITE_SERVER_URL
 
 export const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io(URL, { autoConnect: false })
