da3ca98c51cc4582771877fdf3ccefbc17d3552f
foobar <uagnl25x@anonaddy.me>
2023-12-03 21:34:16 +0530

    Added log to auth middleware

--- backend/src/middleware/auth.ts
+++ backend/src/middleware/auth.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from 'express'
 
 export const isUserAuthenticated = async (req: Request, res: Response, next: NextFunction) => {
+    console.log('credentials are:', req.session.id, req.session.username)
     if (req.session.username && req.session.id) next()
     else {
         res.sendStatus(401)
