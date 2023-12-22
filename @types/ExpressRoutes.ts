4612e3400203520437bf323fabccd44ecb17b8f0
foobar <uagnl25x@anonaddy.me>
2023-12-22 23:25:48 +0530

    new file:   @types/ExpressRoutes.ts

---
+++ @types/ExpressRoutes.ts
@@ -0,0 +1,31 @@
+export const Routes = {
+    get: {
+        messages: '/Messages',
+        roomParticipants: '/room-participants',
+        userRooms: '/user-rooms',
+        userRoom: '/user-room',
+        users: '/users',
+        user: '/user',
+        isUsernameValid: '/is-username-valid',
+        logout: '/logout',
+        sessionStatus: '/session-status',
+    },
+    post: {
+        login: '/login',
+        signup: '/signup',
+        avatar: '/avatar',
+        media: '/Media',
+        privateRoom: '/private-room',
+        group: '/group',
+        participants: '/participants',
+    },
+    put: {
+        messageReadStatus: '/message-read-status',
+        user: '/user',
+        userRoom: '/user-room',
+    },
+    delete: {
+        userRoom: '/user-room',
+        room: '/room',
+    },
+} as const
---
+++ @types/socket.ts
@@ -0,0 +1,67 @@
+import { Socket } from '../frontend/node_modules/socket.io-client'
+import { Message, Room, User, UserRoom, RoomType } from '../prisma/index.d'
+
+export interface ServerToClientEvents {
+    roomMembersChanged: (roomId: Room['roomId'], updatedMemberIds: User['username'][]) => void
+    test: (val?: string) => void
+
+    noArg: () => void
+    // withAck: (d: string, callback: (e: number) => void) => void
+    message: (message: Message, callback: (status: 'ok') => void) => void
+
+    newRoomCreated: (roomId: Room['roomId']) => void
+
+    userProfileUpdated: (newSettings: Partial<User>) => void
+    notification: (roomId: Room['roomId']) => void
+    userLeftRoom: (username: User['username'], roomId: Room['roomId']) => void
+
+    userJoinedRoom: (roomId: Room['roomId'], participants: User[]) => void
+    roomDeleted: (roomId: Room['roomId']) => void
+    typingStatusChanged: (status: TypingStatus, roomId: Room['roomId'], username: User['username']) => void
+    messageDeleted: (messageKey: Message['key'], roomId: Room['roomId']) => void
+    textMessageUpdated: (
+        messageKey: Message['key'],
+        messageContent: Message['content'],
+        roomId: Room['roomId'],
+        editedAt: Date
+    ) => void
+}
+
+export interface InterServerEvents {}
+
+// for socket.on()
+export interface ClientToServerEvents {
+    message: (message: Message, callback: (status: 'ok') => void) => void
+    userProfileUpdated: (newSettings: Partial<User>) => void
+    roomUpdated: (updatedDetails: Partial<Room>) => void
+    notification: (roomId: Room['roomId']) => void
+    // newRoomCreated: (participants: User['username'][], roomId: Room['roomId']) => void
+    newRoomCreated: (paramteres: NewRoomParameters) => void
+    userLeftRoom: (roomId: Room['roomId']) => void
+    userJoinedRoom: (roomId: Room['roomId'], participants: User['username'][]) => void
+    roomDeleted: (roomId: Room['roomId']) => void
+    typingStatusChanged: (status: TypingStatus, roomId: Room['roomId'], username: User['username']) => void
+    messageDeleted: (messageKey: Message['key'], roomId: Room['roomId']) => void
+    textMessageUpdated: (messageKey: Message['key'], messageContent: Message['content'], roomId: Room['roomId']) => void
+}
+
+export type NewRoomParameters =
+    | {
+          roomType: 'private'
+          participant: User['username']
+      }
+    | {
+          roomType: 'group'
+          avatarPath: Room['roomAvatar']
+          displayName: NonNullable<Room['roomDisplayName']>
+          participants: User['username'][]
+      }
+
+export enum TypingStatus {
+    typing = 'typing',
+    notTyping = 'not-typing',
+}
+
+export type Nullable<T> = { [U in keyof T]: null | T[U] }
+
+export type SocketWithCustomEvents = Socket<ServerToClientEvents, ClientToServerEvents>
