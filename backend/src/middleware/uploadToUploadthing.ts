c35ac595c4b3b3a6f3f997a89c9dd620722e355c
foobar <uagnl25x@anonaddy.me>
2023-12-26 00:30:49 +0530

    Added middleware to upload files to uploadthing's S3 service

---
+++ backend/src/middleware/uploadToUploadthing.ts
@@ -0,0 +1,28 @@
+import { NextFunction, Request, Response } from 'express'
+import multer from 'multer'
+import { UploadFileResponse } from 'uploadthing/client'
+import { UTApi } from 'uploadthing/server'
+
+const utapi = new UTApi()
+
+export const uploadMediaToUploadthing = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        if (!req.file) throw new Error('no file to upload')
+
+        const { buffer, mimetype, originalname, size } = req.file
+
+        if (!buffer) throw new Error('empty buffer for file')
+
+        const imageBlob = new Blob([buffer], { type: mimetype })
+
+        const { data, error } = await utapi.uploadFiles(imageBlob, { metadata: { mimetype, name: originalname } })
+
+        if (error || !data) throw new Error('Error uploading file')
+
+        req.mediaUploadData = data
+
+        next()
+    } catch (error) {
+        console.log(error)
+    }
+}
